d8feaba10df0bf4f2d15ca0ea3153e55e4924212
ieow <cwlee0@gmail.com>
2023-09-20 17:04:42 +0800

    fix: mnemonic

--- src/helper/factorSerialization.ts
+++ src/helper/factorSerialization.ts
@@ -1,27 +1,23 @@
-import type { ShareSerializationModule } from "@tkey-mpc/share-serialization";
+import { ShareSerializationModule } from "@tkey-mpc/share-serialization";
 import BN from "bn.js";
-
-import { ICoreKit } from "../interfaces";
 
 /**
  * Converts a mnemonic to a BN.
- * @param tKey - An initialized tKey instance.
  * @param shareMnemonic - The mnemonic to convert.
  * @returns A BN respective to your mnemonic
  */
-export async function mnemonicToKey(MPCCoreKit: ICoreKit, shareMnemonic: string): Promise<BN> {
-  const factorKey = await (MPCCoreKit.tKey.modules.shareSerialization as ShareSerializationModule).deserialize(shareMnemonic, "mnemonic");
+export async function mnemonicToKey(shareMnemonic: string): Promise<BN> {
+  const factorKey = ShareSerializationModule.deserializeMnemonic(shareMnemonic);
   return factorKey;
 }
 
 /**
  * Converts a BN to a mnemonic.
- * @param tKey - An initialized tKey instance.
  * @param shareBN - The BN to convert.
  * @returns A mnemonic respective to your BN
  */
-export async function keyToMnemonic(MPCCoreKit: ICoreKit, shareHex: string): Promise<unknown> {
+export async function keyToMnemonic(shareHex: string): Promise<unknown> {
   const shareBN = new BN(shareHex, "hex");
-  const mnemonic = await (MPCCoreKit.tKey.modules.shareSerialization as ShareSerializationModule).serialize(shareBN, "mnemonic");
+  const mnemonic = ShareSerializationModule.serializeMnemonic(shareBN);
   return mnemonic;
 }
