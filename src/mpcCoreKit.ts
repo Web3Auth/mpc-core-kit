c4bc2d656419874ad310a2f66d5a4a0778258e55
ieow <cwlee0@gmail.com>
2023-09-22 11:54:06 +0800

    fix: delete hashed factorMetadata on reset account use update-metadata for inputing factor

--- src/mpcCoreKit.ts
+++ src/mpcCoreKit.ts
@@ -593,6 +593,9 @@ export class Web3AuthMPCCoreKit implements ICoreKit {
       let factorKey: BN;
       if (this.options.disableHashedFactorKey) {
         factorKey = generateFactorKey().private;
+        // delete previous hashed factorKey if present
+        const hashedFactorKey = getHashedPrivateKey(this.state.oAuthKey, this.options.web3AuthClientId);
+        await this.deleteShareWithFactorKey(hashedFactorKey);
       } else {
         factorKey = getHashedPrivateKey(this.state.oAuthKey, this.options.web3AuthClientId);
       }
@@ -618,7 +621,7 @@ export class Web3AuthMPCCoreKit implements ICoreKit {
       if (await this.checkIfFactorKeyValid(hashedFactorKey)) {
         // Initialize tkey with existing hashed share if available.
         const factorKeyMetadata: ShareStore = await this.getFactorKeyMetadata(hashedFactorKey);
-        await this.tKey.inputShareStoreSafe(factorKeyMetadata);
+        await this.tKey.inputShareStoreSafe(factorKeyMetadata, true);
         await this.tKey.reconstructKey();
         await this.finalizeTkey(hashedFactorKey);
       }
