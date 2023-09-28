b9c7b31661a51befb273671e695c78df1ee31312
Matthias Geihs <matthias@tor.us>
2023-09-28 14:23:40 +0200

    point.ts: add documentation

--- src/point.ts
+++ src/point.ts
@@ -7,19 +7,35 @@ import { CURVE } from "./constants";
 type EllipticPoint = curve.base.BasePoint;
 const ZERO_POINT = CURVE.g.mul(new BN(0)) as EllipticPoint;
 
-// TODO add documentation
+/**
+ * Class `Point` represents an elliptic curve point over curve `CURVE`.
+ */
 export class Point {
   private p: EllipticPoint;
 
+  /**
+   * Constructs a new Point from an elliptic point.
+   * @param p - The elliptic point to be represented.
+   */
   constructor(p: EllipticPoint) {
     this.p = p;
   }
 
+  /**
+   * Creates a new Point from a TKey Point.
+   * @param p - The TKey Point.
+   * @returns The Point encoded by `p`.
+   */
   public static fromTkeyPoint(p: TkeyPoint): Point {
     const ep = CURVE.keyFromPublic({ x: p.x.toString("hex"), y: p.y.toString("hex") }).getPublic();
     return new Point(ep);
   }
 
+  /**
+   * Creates a new Point from an SEC1-encoded byte buffer.
+   * @param buf - The SEC1-encoded point.
+   * @returns The Point encoded by `buf`.
+   */
   public static fromBufferSEC1(buf: Buffer): Point {
     // "elliptic"@6.5.4 can't decode zero point.
     if (buf.length === 1 && buf[0] === 0) {
@@ -30,6 +46,12 @@ export class Point {
     return new Point(p);
   }
 
+  /**
+   * Converts this point to a TKey Point.
+   * @returns A TKey Point representing this point.
+   * @throws If this point cannot be represented by a TKey Point. For example,
+   * if this point encodes the point at infinity.
+   */
   public toTkeyPoint(): TkeyPoint {
     if (this.p.isInfinity()) {
       throw new Error("Point at infinity can't be represented as tkey point.");
@@ -40,6 +62,11 @@ export class Point {
     return new TkeyPoint(x, y);
   }
 
+  /**
+   * Converts this point to a byte buffer in SEC1 format.
+   * @param compressed - Whether to use compressed format.
+   * @returns The SEC1-encoded representation of the point.
+   */
   public toBufferSEC1(compressed: boolean): Buffer {
     // "elliptic"@6.5.4 can't encode zero point.
     if (this.p.isInfinity()) {
