468fa21dddabdc68defea5ffda727e3fef7a8cd1
ieow <cwlee0@gmail.com>
2023-10-11 12:24:18 +0800

    fix: update mockLogin

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import BN from "bn.js";
+import jwt, { Algorithm } from "jsonwebtoken";
 
 import { parseToken, Web3AuthMPCCoreKit } from "../src";
 if (global.navigator === undefined) {
@@ -11,7 +12,7 @@ console.log(navigator);
 
 console.log("before all");
 
-export const mockLogin = async (email: string) => {
+export const mockLogin2 = async (email: string) => {
   const req = new Request("https://li6lnimoyrwgn2iuqtgdwlrwvq0upwtr.lambda-url.eu-west-1.on.aws/", {
     method: "POST",
     headers: {
@@ -39,4 +40,28 @@ export const criticalResetAccount = async (coreKitInstance: Web3AuthMPCCoreKit):
     privKey: new BN(coreKitInstance.metadataKey!, "hex"),
     input: { message: "KEY_NOT_FOUND" },
   });
+};
+
+const privateKey = "MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCCD7oLrcKae+jVZPGx52Cb/lKhdKxpXjl9eGNa1MlY57A==";
+const jwtPrivateKey = `-----BEGIN PRIVATE KEY-----\n${privateKey}\n-----END PRIVATE KEY-----`;
+const alg: Algorithm = "ES256";
+
+export const mockLogin = async (email: string) => {
+  const iat = Math.floor(Date.now() / 1000);
+  const payload = {
+    iss: "torus-key-test",
+    aud: "torus-key-test",
+    name: email,
+    email,
+    scope: "email",
+    iat,
+    eat: iat + 120,
+  };
+
+  const algo = {
+    expiresIn: 120,
+    algorithm: alg,
+  };
+
+  return jwt.sign(payload, jwtPrivateKey, algo);
 };
