1651a77805eccdf356ea23de85304e1e3e868243
Matthias Geihs <matthias@tor.us>
2024-05-23 12:23:44 +0800

    :construction: debug import recovery test

--- tests/importRecovery.spec.ts
+++ tests/importRecovery.spec.ts
@@ -19,52 +19,32 @@ type ImportKeyTestVariable = {
 
 const storageInstance = new MemoryStorage();
 export const ImportTest = async (testVariable: ImportKeyTestVariable) => {
+  async function newCoreKitInstance(email: string) {
+    return newCoreKitLogInInstance({
+      network: WEB3AUTH_NETWORK.DEVNET,
+      manualSync: testVariable.manualSync,
+      email: email,
+      storageInstance,
+      tssLib: testVariable.tssLib,
+    });
+  }
+
+  async function resetAccount(email: string) {
+    const kit = await newCoreKitInstance(email);
+    await criticalResetAccount(kit);
+    await kit.logout();
+    await new AsyncStorage(kit._storageKey, storageInstance).resetStore();
+  }
+
   test(`import recover tss key : ${testVariable.manualSync}`, async function (t) {
     const beforeTest = async () => {
-      const instance = await newCoreKitLogInInstance({
-        network: WEB3AUTH_NETWORK.DEVNET,
-        manualSync: false,
-        email: testVariable.email,
-        storageInstance,
-        tssLib: testVariable.tssLib,
-      });
-      await criticalResetAccount(instance);
-      await instance.logout();
-
-      const instance2 = await newCoreKitLogInInstance({
-        network: WEB3AUTH_NETWORK.DEVNET,
-        manualSync: false,
-        email: testVariable.importKeyEmail,
-        storageInstance,
-        tssLib: testVariable.tssLib,
-      });
-      await criticalResetAccount(instance2);
-      await instance2.logout();
-      await new AsyncStorage(instance2._storageKey, storageInstance).resetStore();
+      await resetAccount(testVariable.email);
+      await resetAccount(testVariable.importKeyEmail);
     };
 
     await beforeTest();
     await t.test("#recover Tss key using 2 factors key, import tss key to new oauth login", async function () {
-      const { idToken, parsedToken } = await mockLogin(testVariable.email);
-
-      const idTokenLoginParams = {
-        verifier: "torus-test-health",
-        verifierId: parsedToken.email,
-        idToken,
-      } as IdTokenLoginParams;
-
-      const coreKitInstance = new Web3AuthMPCCoreKit({
-        web3AuthClientId: "torus-key-test",
-        web3AuthNetwork: WEB3AUTH_NETWORK.DEVNET,
-        baseUrl: "http://localhost:3000",
-        uxMode: "nodejs",
-        tssLib: testVariable.tssLib,
-        storage: new MemoryStorage(),
-        manualSync: testVariable.manualSync,
-      });
-
-      await coreKitInstance.init();
-      await coreKitInstance.loginWithJWT(idTokenLoginParams);
+      const coreKitInstance = await newCoreKitInstance(testVariable.email);
 
       // Create 2 factors which will be use to recover tss key
       const factorKeyDevice = await coreKitInstance.createFactor({
@@ -79,46 +59,20 @@ export const ImportTest = async (testVariable: ImportKeyTestVariable) => {
         await coreKitInstance.commitChanges();
       }
 
+      // Export key and logout.
       const exportedTssKey1 = await coreKitInstance._UNSAFE_exportTssKey();
-      // recover key
-      // reinitalize corekit
       await coreKitInstance.logout();
-      // BrowserStorage.getInstance("memory").resetStore();
 
+      // recover key
       const recoveredTssKey = await coreKitInstance._UNSAFE_recoverTssKey([factorKeyDevice, factorKeyRecovery]);
       assert.strictEqual(recoveredTssKey, exportedTssKey1);
-
-      // relogin to reset account
-      await coreKitInstance.init();
-      const localToken = await mockLogin2(testVariable.email);
-      idTokenLoginParams.idToken = localToken.idToken;
-      await coreKitInstance.loginWithJWT(idTokenLoginParams);
-      await criticalResetAccount(coreKitInstance);
+      
+      // reset account
+      // await criticalResetAccount(coreKitInstance);
       // BrowserStorage.getInstance("memory").resetStore();
 
-      // reinitialize corekit
-      const newEmail = testVariable.importKeyEmail;
-      const newLogin = await mockLogin(newEmail);
-
-      const newIdTokenLoginParams = {
-        verifier: "torus-test-health",
-        verifierId: newLogin.parsedToken.email,
-        idToken: newLogin.idToken,
-        importTssKey: recoveredTssKey,
-      } as IdTokenLoginParams;
-
-      const coreKitInstance2 = new Web3AuthMPCCoreKit({
-        web3AuthClientId: "torus-key-test",
-        web3AuthNetwork: WEB3AUTH_NETWORK.DEVNET,
-        baseUrl: "http://localhost:3000",
-        uxMode: "nodejs",
-        tssLib: testVariable.tssLib,
-        storage: new MemoryStorage(),
-      });
-
-      await coreKitInstance2.init();
-      await coreKitInstance2.loginWithJWT(newIdTokenLoginParams);
-
+      // initialize new instance
+      const coreKitInstance2 = await newCoreKitInstance(testVariable.importKeyEmail);
       if (testVariable.manualSync) {
         await coreKitInstance2.commitChanges();
       }
@@ -129,26 +83,7 @@ export const ImportTest = async (testVariable: ImportKeyTestVariable) => {
       assert.strictEqual(exportedTssKey, recoveredTssKey);
 
       // reinitialize corekit
-      const newEmail3 = testVariable.importKeyEmail;
-      const newLogin3 = await mockLogin(newEmail3);
-
-      const newIdTokenLoginParams3 = {
-        verifier: "torus-test-health",
-        verifierId: newLogin3.parsedToken.email,
-        idToken: newLogin3.idToken,
-      } as IdTokenLoginParams;
-
-      const coreKitInstance3 = new Web3AuthMPCCoreKit({
-        web3AuthClientId: "torus-key-test",
-        web3AuthNetwork: WEB3AUTH_NETWORK.DEVNET,
-        baseUrl: "http://localhost:3000",
-        uxMode: "nodejs",
-        tssLib: testVariable.tssLib,
-        storage: new MemoryStorage(),
-      });
-
-      await coreKitInstance3.init();
-      await coreKitInstance3.loginWithJWT(newIdTokenLoginParams3);
+      const coreKitInstance3 = await newCoreKitInstance(testVariable.importKeyEmail);
       const tssPubkey = bufferToElliptic(coreKitInstance3.getPubKey());
 
       const exportedTssKey3 = await coreKitInstance3._UNSAFE_exportTssKey();
