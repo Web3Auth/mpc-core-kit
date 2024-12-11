f5784fced9cfcc7d25b9099507384ccc9f25bcc9
Guru <guru@tor.us>
2024-12-11 09:57:49 +0530

    fix: css

--- demo/redirect-flow-example/src/App.tsx
+++ demo/redirect-flow-example/src/App.tsx
@@ -1,4 +1,3 @@
-import "../global.css";
 import { useEffect, useState } from "react";
 import {
   Web3AuthMPCCoreKit,
--- demo/redirect-flow-example/src/index.tsx
+++ demo/redirect-flow-example/src/index.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import "../global.css";
 
 import ReactDOM from "react-dom/client";
 
