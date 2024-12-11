cad01c72a62736f2c3748d48dd45a7fff0fd1cab
Guru <guru@tor.us>
2024-12-11 10:30:58 +0530

    fix: post config

--- demo/redirect-flow-example/vite.config.js
+++ demo/redirect-flow-example/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       stream: 'stream-browserify',
