1a4940ce1cdef0dbb1d33ebd49d9b28f428a751a
Guru <guru@tor.us>
2024-12-11 11:23:02 +0530

    fix: tailwind config

--- demo/redirect-flow-example/tailwind.config.js
+++ demo/redirect-flow-example/tailwind.config.js
@@ -5,10 +5,12 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
@@ -76,70 +78,6 @@ export default {
       },
     },
     extend: {
-      colors: {
-        transparent: colors.transparent,
-        app: {
-          onPrimary: "var(--app-on-primary)",
-          primary: {
-            900: "var(--app-primary-900)",
-            800: "var(--app-primary-800)",
-            700: "var(--app-primary-700)",
-            600: "var(--app-primary-600)",
-            500: "var(--app-primary-500)",
-            400: "var(--app-primary-400)",
-            300: "var(--app-primary-300)",
-            200: "var(--app-primary-200)",
-            100: "var(--app-primary-100)",
-            50: "var(--app-primary-50)",
-          },
-          gray: {
-            900: "var(--app-gray-900)",
-            800: "var(--app-gray-800)",
-            700: "var(--app-gray-700)",
-            600: "var(--app-gray-600)",
-            500: "var(--app-gray-500)",
-            400: "var(--app-gray-400)",
-            300: "var(--app-gray-300)",
-            200: "var(--app-gray-200)",
-            100: "var(--app-gray-100)",
-            50: "var(--app-gray-50)",
-          },
-          blue: {
-            900: "var(--app-blue-900)",
-            800: "var(--app-blue-800)",
-            700: "var(--app-blue-700)",
-            600: "var(--app-blue-600)",
-            500: "var(--app-blue-500)",
-            400: "var(--app-blue-400)",
-            300: "var(--app-blue-300)",
-            200: "var(--app-blue-200)",
-            100: "var(--app-blue-100)",
-            50: "var(--app-blue-50)",
-          },
-          light: {
-            "surface-main": "var(--app-gray-100)",
-            surface1: "var(--app-white)",
-            surface2: "var(--app-gray-50)",
-            surface3: "var(--app-gray-100)",
-            surface4: "var(--app-gray-200)",
-          },
-          dark: {
-            "surface-main": "var(--app-gray-900)",
-            surface1: "var(--app-black)",
-            surface2: "var(--app-gray-900)",
-            surface3: "var(--app-gray-800)",
-            surface4: "var(--app-gray-700)",
-          },
-          success: "var(--app-success)",
-          warning: "var(--app-warning)",
-          error: "var(--app-error)",
-          info: "var(--app-info)",
-          white: "var(--app-white)",
-          black: "var(--app-black)",
-          alertDark: "var(--app-alertDark)",
-          blueBorder: "var(--app-blueBorder)",
-        },
-      },
       transitionProperty: {
         height: "height",
         spacing: "margin, padding",
@@ -248,5 +186,4 @@ export default {
     tailwindCssVariables,
     containerQueries,
   ],
-  darkMode: "class",
 };
