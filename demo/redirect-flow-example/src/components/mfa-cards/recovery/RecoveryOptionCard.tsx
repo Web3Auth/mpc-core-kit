e87f0bd2e8ee9f27967dbcbe6360da1b8201115e
Guru <guru@tor.us>
2024-12-11 12:53:26 +0530

    feat: added critical reset

--- demo/redirect-flow-example/src/components/mfa-cards/recovery/RecoveryOptionCard.tsx
+++ demo/redirect-flow-example/src/components/mfa-cards/recovery/RecoveryOptionCard.tsx
@@ -2,13 +2,34 @@ import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../Button";
 import { Card } from "../../Card";
+import { useCoreKit } from "../../../composibles/useCoreKit";
+import { BN } from "bn.js";
+import { COREKIT_STATUS } from "@web3auth/mpc-core-kit";
 
 const RecoveryOptionsCard: React.FC = () => {
   const navigate = useNavigate();
+  const { coreKitInstance, setCoreKitStatus } = useCoreKit();
 
   const handleRecoveryOption = (option: string) => {
     navigate(`/verify-${option}`);
   };
+
+  const criticalReset = async () => {
+    if (!coreKitInstance) {
+      throw new Error("coreKitInstance is not set");
+    }
+    //@ts-ignore
+    // if (selectedNetwork === WEB3AUTH_NETWORK.MAINNET) {
+    //   throw new Error("reset account is not recommended on mainnet");
+    // }
+    await coreKitInstance.tKey.storageLayer.setMetadata({
+      privKey: new BN(coreKitInstance.state.postBoxKey!, "hex"),
+      input: { message: "KEY_NOT_FOUND" },
+    });
+    await coreKitInstance.logout();
+    setCoreKitStatus(COREKIT_STATUS.NOT_INITIALIZED)
+    navigate("/");
+  }
 
   return (
     <div className="w-full flex flex-col justify-center h-[90%] items-center bg-app-gray-100 dark:bg-app-gray-900">
@@ -23,6 +44,9 @@ const RecoveryOptionsCard: React.FC = () => {
           </Button>
           <Button className="w-full mb-4" variant="primary" onClick={() => handleRecoveryOption("password")}>
             Password
+          </Button>
+          <Button className="w-full mb-4" variant="primary" onClick={() => criticalReset()}>
+            Critical Reset
           </Button>
         </div>
       </Card>
