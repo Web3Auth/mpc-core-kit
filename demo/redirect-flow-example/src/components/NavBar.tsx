5805dc85daf3fca23f7e9694e77797ed2a8fa27d
Guru <guru@tor.us>
2024-12-12 13:58:03 +0530

    fix: navbar for corekit

--- demo/redirect-flow-example/src/components/NavBar.tsx
+++ demo/redirect-flow-example/src/components/NavBar.tsx
@@ -39,15 +39,15 @@ const Header: React.FC = () => {
   return (
     <header className="w-full relative">
       <div className="py-4 px-4 sm:py-6 sm:px-8 flex items-center justify-between bg-app-white dark:bg-app-gray-800 w-full">
-        <img src="https://demo.web3auth.io/assets/logo-Dbyf1Tt2.svg" alt="web3auth demo logo" className="cursor-pointer h-8 sm:!h-12 w-auto dark:hidden" />
-        <img src="/images/logo-light.svg" alt="web3auth demo logo" className="cursor-pointer h-8 sm:!h-12 w-auto hidden dark:block" />
+        <div className="flex flex-row gap-x-1.5">
+          <img src="https://web3auth.io/images/web3authlog.png" alt="web3auth demo logo" className="cursor-pointer h-8 sm:!h-12 w-auto dark:hidden" />
+          <div className="flex flex-col">
+            <span className="text-app-gray-900 dark:text-app-white font-semibold text-xl">Web3auth</span>
+            <span className="text-app-gray-500 dark:text-app-gray-400 text-sm font-bold">MPC Core Kit Demo</span>
+          </div>
+        </div>
         {!isLogin ? (
-          <Button
-            id="w3a-documentation"
-            href="https://web3auth.io/docs"
-            rel="noopener noreferrer"
-            className="!h-9 sm:!h-10"
-          >
+          <Button id="w3a-documentation" href="https://web3auth.io/docs" rel="noopener noreferrer" className="!h-9 sm:!h-10">
             Documentation
           </Button>
         ) : (
